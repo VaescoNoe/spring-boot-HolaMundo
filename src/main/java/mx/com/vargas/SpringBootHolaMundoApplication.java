d639ccfe7ccfaeb7a896d50a0bb1bdb8694bb7d7
Noe <VaescoNoe@gmail.com>
2019-03-03 21:27:51 -0600

    Inicio de proyecto

---
+++ src/main/java/mx/com/vargas/SpringBootHolaMundoApplication.java
@@ -0,0 +1,13 @@
+package mx.com.vargas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHolaMundoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHolaMundoApplication.class, args);
+	}
+
+}
